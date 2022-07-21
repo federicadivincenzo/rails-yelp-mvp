163e0063fc2b5fa93985fbae4383e16b2175469c
Federica Di Vincenzo <federicadivincenzo5@gmail.com>
2022-07-21 17:39:46 +0200

    styling added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,8 +8,18 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.delete
+    redirect_to restaurant_path(@review.restaurant)
+
   end
 
   private
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,8 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian),
+    message: "%{value} is not a valid category" }
 
   has_many :reviews, dependent: :destroy
 end
